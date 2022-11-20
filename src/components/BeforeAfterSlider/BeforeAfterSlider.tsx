b2a153bb69a131164ff292f9689827b80a9aea64
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-20 22:20:27 +0200

    Finished BeforeAfter Slider

--- src/components/BeforeAfterSlider/BeforeAfterSlider.tsx
+++ src/components/BeforeAfterSlider/BeforeAfterSlider.tsx
@@ -2,15 +2,20 @@ import styles from "./BeforeAfterSlider.module.scss";
 import data from "data/data";
 import Image from "next/image";
 import combineClasses from "helpers/combineClasses";
+import { useState } from "react";
 
 const BeforeAfterSlider = () => {
+  const [sliderValue, setSliderValue] = useState(50);
   const beforeImage = data.beforeImage;
   const afterImage = data.afterImage;
 
   return (
     <section className={styles.wrapper}>
-      <div className={styles.slider}>
-        <div className={combineClasses(styles.imageWrapper, styles.before)}>
+      <div className={styles.sliderWrapper}>
+        <div
+          className={combineClasses(styles.imageWrapper, styles.before)}
+          style={{ width: `${sliderValue}%` }}
+        >
           <Image
             src={beforeImage.src}
             alt={beforeImage.name}
@@ -26,6 +31,20 @@ const BeforeAfterSlider = () => {
             fill
           />
         </div>
+
+        <div
+          className={styles.sliderButton}
+          style={{ left: `calc(${sliderValue}% - 18px)` }}
+        />
+        <input
+          type="range"
+          min="1"
+          max="100"
+          value={sliderValue}
+          className={styles.slider}
+          onChange={(e) => setSliderValue(parseInt(e.target.value))}
+          name="slider"
+        />
       </div>
     </section>
   );
