001ba90b6a6ed909482a09c3cc338aecc8114905
Mihai Ursu <ursu.mihai.mail@gmail.com>
2022-11-20 17:34:27 +0200

    Fixed css links

---
+++ src/components/BeforeAfterSlider/BeforeAfterSlider.tsx
@@ -0,0 +1,11 @@
+import styles from "./BeforeAfterSlider.module.scss";
+
+const BeforeAfterSlider = () => {
+  return (
+    <section>
+      <h1>Before and after Slider</h1>
+    </section>
+  );
+};
+
+export default BeforeAfterSlider;
